51da1b1c828e765f74df5d761e6f921e5cc813d0
Hoda Akrami <Hakrami3257@conestogac.on.ca>
2023-04-13 14:22:25 -0400

    added custome alert

--- frontend/src/modules/orders.js
+++ frontend/src/modules/orders.js
@@ -4,62 +4,93 @@ export default {
   components: {
     ConfirmationDialog
   },
-    data() {
-      return {
-        sortAscending: true,
-        NameOfPage: '',
-        isPopupVisible: false,
-        orders: [],
-        orderDisplayed: null,
-        isAdmin: false,
-        openReturnConfirmDialog: false,
-        isReturnedDisabled: false,
-        orderIdToUpdate: null
-      };
+  computed: {
+    filteredOrders() {
+      if (!this.searchText) {
+        return this.orders;
+      }
+      console.log('in computed')
+      const searchTextLower = this.searchText.toLowerCase();
+      return this.orders.filter(order => {
+        return (
+          order.id.toLowerCase().includes(searchTextLower) ||
+          order.book.title.toLowerCase().includes(searchTextLower) ||
+          order.book.author_name.toLowerCase().includes(searchTextLower)
+        );
+      });
+    }
+  },
+  data() {
+    return {
+      sortAscending: true,
+      NameOfPage: '',
+      isPopupVisible: false,
+      orders: [],
+      orderDisplayed: null,
+      isAdmin: false,
+      openReturnConfirmDialog: false,
+      isReturnedDisabled: false,
+      orderIdToUpdate: null,
+      searchText: ""
+    };
+  },
+  async mounted () {
+    await this.loadOrders()
+    window.addEventListener('load', () => {
+      const url = new URL(window.location.href);
+      const alertMessage = url.searchParams.get('alertMessage');
+      const alertType = url.searchParams.get('alertType');
+
+      if (alertMessage && alertType) {
+        this.$refs.alert.showAlert(alertType, alertMessage, 'Success');
+        url.searchParams.delete('alertMessage');
+        url.searchParams.delete('alertType');
+        window.history.replaceState({}, '', url);
+      }
+    });
+  },
+  methods: {
+    async loadOrders() {
+      let ordersRsp;
+      // Check if the user is an admin or a regular user
+      if (localStorage.getItem("isAdmin").toLowerCase() == "true") { // Replace userIsAdmin with the actual condition to check if the user is an admin
+          this.isAdmin = true
+          ordersRsp = await orderApi.getAllOrders();
+      } else {
+          ordersRsp = await orderApi.getAllOrdersForUser();
+      }
+      this.orders = ordersRsp.data;
+      console.log('this.orders ', this.orders)
     },
-    async mounted () {
-      await this.loadOrders()
+    sortData() {
+      let sortedDataArray = this.orders.slice()
+      sortedDataArray.sort((a, b) => {
+          if (this.sortAscending) {
+          return a.book.title.localeCompare(b.book.title)
+          } else {
+          return b.book.title.localeCompare(a.book.title)
+          }
+      })
+      this.orders = sortedDataArray
+      this.sortAscending = !this.sortAscending
     },
-    methods: {
-      async loadOrders() {
-        let ordersRsp;
-        // Check if the user is an admin or a regular user
-        if (localStorage.getItem("isAdmin").toLowerCase() == "true") { // Replace userIsAdmin with the actual condition to check if the user is an admin
-            this.isAdmin = true
-            ordersRsp = await orderApi.getAllOrders();
-        } else {
-            ordersRsp = await orderApi.getAllOrdersForUser();
-        }
-        this.orders = ordersRsp.data;
-        console.log('this.orders ', this.orders)
-      },
-      sortData() {
-        let sortedDataArray = this.orders.slice()
-        sortedDataArray.sort((a, b) => {
-            if (this.sortAscending) {
-            return a.book.title.localeCompare(b.book.title)
-            } else {
-            return b.book.title.localeCompare(a.book.title)
+    openReturnDialog(index){
+      this.orderIdToUpdate = this.orders[index].id
+      this.openReturnConfirmDialog = true
+    },
+    updateReturnStatus() {
+      this.isReturnedDisabled = true
+      if (this.orderIdToUpdate !== null) {
+          orderApi.updateOrder(this.orderIdToUpdate).then(response => {
+            if(response && response.status === 200){            
+              const url = new URL(window.location.href);
+              url.searchParams.set('alertMessage', 'Successfully Returned the Book!');
+              url.searchParams.set('alertType', 'success');
+              window.location.href = url.toString();
             }
-        })
-        this.orders = sortedDataArray
-        this.sortAscending = !this.sortAscending
-      },
-      openReturnDialog(index){
-        this.orderIdToUpdate = this.orders[index].id
-        this.openReturnConfirmDialog = true
-      },
-      updateReturnStatus() {
-        this.isReturnedDisabled = true
-        if (this.orderIdToUpdate !== null) {
-            orderApi.updateOrder(this.orderIdToUpdate).then(response => {
-                if(response && response.status === 200){
-                
-                location.reload()
-                }
-            })
-            } else {
-            console.warn('No order ID to update!')
+          })
+          } else {
+          console.warn('No order ID to update!')
         }
       }
     }
--- frontend/src/views/OrdersView.vue
+++ frontend/src/views/OrdersView.vue
@@ -1,5 +1,9 @@
 <template>
 <div>
+  <vue-basic-alert
+     :duration="300"
+     :closeIn="2500"
+     ref="alert" />
    <ConfirmationDialog
     style="align-content: center"
     v-if="openReturnConfirmDialog"
@@ -13,6 +17,20 @@
   <div class="py-4 container-fluid">
     <div class="card scrollable">
       <div class="card-body">
+        <!-- Search form -->
+        <div style="display:inline-block; float:right;" class="d-none d-md-flex input-group w-auto my-auto navbar-nav ms-auto d-flex flex-row">
+            <input
+              v-model="searchText"
+              autocomplete="off"
+              type="search"
+              class="form-control rounded"
+              placeholder='Search'
+              style="min-width: 225px"
+            />
+            <span class="input-group-text border-0">
+              <i class="fas fa-search"></i>
+            </span>
+          </div>
         <div id="table" class="table-editable">
           <table class="table table-responsive-md table-striped text-center">
             <thead>
@@ -38,7 +56,7 @@
                 <td>{{ this.orders.length ? this.orders[index].checkout_date : '' }}</td>
                 <td>{{ this.orders.length ? this.orders[index].final_return_date : '' }}</td>
                 <td>{{ this.orders.length ? this.orders[index].returned_date : '' }}</td>
-                <td>
+                <td v-if="isAdmin">
                   <span v-if="this.orders[index].returned_date === null">
                     <span @click="updateReturnStatus()"></span>
                     <span @click="openReturnDialog(index)">
